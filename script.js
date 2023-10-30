29e02f1583a015cf702d4a11c9b78dccbef0706e
ITACHI2704 <krishankumar39848@gmail.com>
2023-10-30 19:26:50 +0500

    script adder

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ function closemenu(){
     sidemenu.style.right="-200px";
 }
 
+
+// ---------contact form------------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxAJgfjxOorQ9P4-hmSwnUZsrr0d_ivrVeyDb3jFbQ96Ybrw2z7wG4EK_EbT2iqazA8/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        msg.innerHTML="Message sent succesfully."
+        setTimeout(function(){
+            msg.innerHTML=""
+        },2000)
+        form.reset()
+    } )
+    .catch(error => console.error('Error!', error.message))
+})
